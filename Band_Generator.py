98e717278608ffdacc983f09e7c641fa4b86dc43
Rick.Ruler <91057197+Pharcydes@users.noreply.github.com>
2022-04-13 19:27:45 -0400

    Update and rename Untitled-1.py to Band_Generator.py

---
+++ Band_Generator.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("Hello how are you today we're going to ask you a few questions today to get a better understanding of what type of band you want to be named.")
+#2. Ask the user for the city that they grew up in.
+city = input("What City are you from? \n")
+#3. Ask the user for the name of a pet.
+pet = input("What is your first pet's name? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print(city + "" + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
+
+# This is a band generator program from Angela Yu's 100 days of Code Day 1 project. 
